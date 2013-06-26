20164844b7ad73d0fa508944e6c329aa17c18e1d
Rok Mandeljc <rok.mandeljc@gmail.com>
2013-06-26 20:15:43 +0200

    The project will be released under GPLv3

--- pipeline/StereoCalibrationPattern.cpp
+++ pipeline/StereoCalibrationPattern.cpp
@@ -1,3 +1,22 @@
+/*
+ * Stereo Pipeline: calibration pattern
+ * Copyright (C) 2013 Rok Mandeljc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 #include "StereoCalibrationPattern.h"
 
 #include <opencv2/calib3d/calib3d.hpp>
